29a07115b970e5683ba9d583fd522b4b623292c2
Franz Ivan De Villa <franzivan.devilla@gmail.com>
2025-09-26 09:54:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const helmet = require('helmet');
 const { connectDB } = require('./db');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+
+// Only require test-runner in test environment
+let runner;
+if (process.env.NODE_ENV === 'test') {
+  try {
+    runner = require('./test-runner');
+  } catch (e) {
+    console.log('Warning: test-runner not available:', e.message);
+  }
+}
 
 // Connect to database
 connectDB();
@@ -80,7 +89,7 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if(process.env.NODE_ENV==='test' && runner) {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
